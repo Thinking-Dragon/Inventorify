6ae8d4f60e08b0a9435e274a216ac8bb4b4da6f2
Thinking-Dragon <cfmgp@hotmail.com>
2022-01-17 16:06:30 -0500

    Remove api_routes file

--- backend_api/source/api_routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (request, response) => {
-    response.send('You are in the api');
-});
-
-module.exports = router;
