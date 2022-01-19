a701406facaf7ebe8ccbf59455b387f4821571f0
Thinking-Dragon <cfmgp@hotmail.com>
2022-01-19 16:53:24 -0500

    Implement get all inventory items endpoint with entends parameter

--- backend_api/source/api/services/InventoryService.ts
+++ backend_api/source/api/services/InventoryService.ts
@@ -3,19 +3,33 @@ import 'reflect-metadata';
 import InventoryItem from '../entities/InventoryItem';
 import RelationalDatabaseService from './database/RelationalDatabaseService';
 import SERVICE_TYPES from './ServiceTypes';
+import * as fs from 'fs';
+import * as ItemInflater from '../inflaters/InventoryItemInflater';
 
 @injectable()
 class InventoryService {
+    private static readonly GET_ALL_INVENTORY_ITEMS_EXTENDED_SCRIPT: string = '../persistence/get_all_inventory_items_extended.sql';
+    private static readonly GET_ALL_INVENTORY_ITEMS_SCRIPT: string = '../persistence/get_all_inventory_items.sql';
+
     constructor(@inject(SERVICE_TYPES.RelationalDatabaseService) databaseService: RelationalDatabaseService) {
         this._databaseService = databaseService;
     }
 
-    async getAllItems(): Promise<Array<InventoryItem>> {
-        const query: string = `
-            SELECT *
-            FROM InventoryItems
-        `;
-        return await this._databaseService.all(query);
+    async getAllItems(extend: boolean = true): Promise<Array<InventoryItem>> {
+        let query: string;
+        
+        if(extend) query = fs.readFileSync(InventoryService.GET_ALL_INVENTORY_ITEMS_EXTENDED_SCRIPT).toString();
+        else query = fs.readFileSync(InventoryService.GET_ALL_INVENTORY_ITEMS_SCRIPT).toString();
+
+        const queryResult = await this._databaseService.all(query);
+
+        const rows: Array<InventoryItem> = [];
+        for(const row of queryResult) {
+            if(extend) rows.push(ItemInflater.inflateFlatItem(row));
+            else rows.push(ItemInflater.decorateFlatItem(row));
+        }
+
+        return rows;
     }
 
     getOneItem(id: number): InventoryItem {
@@ -29,6 +43,8 @@ class InventoryService {
     modifyItem(item: InventoryItem): InventoryItem {
         throw new Error('Method not implemented.');
     }
+
+    
 
     private _databaseService: RelationalDatabaseService;
 }
