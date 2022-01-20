045a4df53904e4c0839bacc4abf2ec94ba1546b1
Thinking-Dragon <cfmgp@hotmail.com>
2022-01-20 10:58:37 -0500

    Add cors dependency to api

--- backend_api/source/WebServer.ts
+++ backend_api/source/WebServer.ts
@@ -1,20 +1,26 @@
 import * as express_module from 'express';
 import express from 'express';
 import Controller from './api/controllers/Controller';
+import cors from 'cors';
 
 class WebServer {
     constructor(controllers: Array<Controller>, port: number) {
         this.application = express();
         this.port = port;
 
-        this.initializeControllers(controllers);
+        this.bindMiddlewares();
+        this.bindControllers(controllers);
     }
 
     public listen(): void {
         this.application.listen(this.port, () => console.log(`Listening on port ${this.port}`));
     }
 
-    private initializeControllers(controllers: Array<Controller>) {
+    private bindMiddlewares(): void {
+        this.application.use(cors());
+    }
+
+    private bindControllers(controllers: Array<Controller>) {
         controllers.forEach(controller => controller.useApplication(this.application));
     }
 
